71da8c59267bfaafa510ac4b7f4c7c2e62d0d660
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-07 13:41:22 -0800

    gdfsfs

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+app.get('/test', (req, res) => res.send('Testing 1, 2, 3'));
 
 // app.get('/books', (req, res) => {
 //   client.query(`SELECT * from books;`)
@@ -26,24 +26,24 @@ app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-loadDB();
-
-function loadBooks() {
-  fs.readFile('../book-list-client/data/books.json', function(err, fd) {
-    JSON.parse(fd.toString()).forEach(function(ele) {
-      client.query(
-        'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
-        [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
-      )
-    })
-  })
-}
-
-function loadDB() {
-  client.query(`
-    CREATE TABLE IF NOT EXISTS
-    books(id SERIAL PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), isbn VARCHAR(255), image_url VARCHAR(255), description TEXT NOT NULL);
-    `)
-
-    .then(loadBooks());
-}
+// loadDB();
+//
+// function loadBooks() {
+//   fs.readFile('../book-list-client/data/books.json', function(err, fd) {
+//     JSON.parse(fd.toString()).forEach(function(ele) {
+//       client.query(
+//         'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
+//         [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
+//       )
+//     })
+//   })
+// }
+//
+// function loadDB() {
+//   client.query(`
+//     CREATE TABLE IF NOT EXISTS
+//     books(id SERIAL PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), isbn VARCHAR(255), image_url VARCHAR(255), description TEXT NOT NULL);
+//     `)
+//
+//     .then(loadBooks());
+// }
